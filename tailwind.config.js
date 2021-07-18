e97d886fd84a248fa16fba4cccdc2ddf26539581
Justin Byrne <theskaterjesus@gmail.com>
2021-07-18 21:48:31 +0100

    Fixed image colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,14 @@ module.exports = {
       fontFamily: {
         inter: 'Inter, sans-serif',
         deca: 'Lexend Deca, sans-serif',
-
       },
       colors: {
         darkBlue: "hsl(233, 47%, 7%)",
         desaturatedBlue: "hsl(244, 38%, 16%)",
         accent: "hsl(277, 64%, 61%)",
+      },
+      brightness: {
+        '130': '1.3',
       },
     },
   },
